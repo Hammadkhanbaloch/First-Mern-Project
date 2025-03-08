20681febde1f4cb078c5dbd347a683c100b4ae00
Hammadkhanbaloch <cosc232101045@kfueit.edu.pk>
2025-03-08 22:53:16 +0500

    clientside Listing

--- src/Pages/listing.jsx
+++ src/Pages/listing.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 
 export default function CreateListing() {
+    const [formData, setFormData] = React.useState({
+        name: '',
+        description: '',
+        address: '',
+        sell: false,
+        rent: false,
+        parking: false,
+        furnished: false,
+        offer: false,
+        bedRoom: 0,
+        bathRoom: 0,
+        regularPrice: 0,
+        discountPrice: 0,
+        file: []
+    });
+    const handlechange=(e)=>{
+      if(e.target.id==='sell'||e.target.id==='rent'||e.target.id==='parking'||e.target.id==='furnished'||e.target.id==='offer'){
+        setFormData({...formData,[e.target.id]:e.target.checked})
+      }
+        else{
+          setFormData({...formData,[e.target.id]:e.target.value})
+    }
+}
+console.log(formData)
   return (
-    <main className="p-3 max-w-4xl max-auto ">
+    <main className="p-4 max-w-4xl mx-auto">
         <div className="text-center text-3xl my-7 font-semibold">
         <h1>Create Listing</h1>
         </div>
@@ -10,50 +34,50 @@ export default function CreateListing() {
             <form className="flex flex-col sm:">
                 <div className="flex flex-col sm:flex-row p-2 gap-4">
                     <div className="flex flex-col gap-3 flex-1">
-                    <input type="text" placeholder="Name" id="name" className='p-3 rounded-lg'></input>
-                    <input type="text" placeholder="description" id="description" className='p-3 rounded-lg'></input>
-                    <input type="text" placeholder="Address" id="address" className='p-3 rounded-lg'></input>
+                    <input type="text" placeholder="Name" id="name" value={formData.name} onChange={handlechange} className='p-3 rounded-lg'></input>
+                    <input type="text" placeholder="description"  value={FormData.description} id="description" onChange={handlechange} className='p-3 rounded-lg'></input>
+                    <input type="text" placeholder="Address" id="address" value={formData.address} onChange={handlechange} className='p-3 rounded-lg'></input>
                     </div>
                     <div className='flex gap-6 flex-wrap'>
                         <div className='flex items-center gap-3'>
-                            <input type="checkbox" id="sell" className="w-5"/>
+                            <input type="checkbox" checked={formData.sell} onChange={handlechange} id="sell" className="w-5"/>
                             <span>Sell</span>
                         </div>
                         <div className='flex items-center gap-3'>
-                            <input type="checkbox" id="rent" className="w-5"/>
+                            <input type="checkbox" checked={formData.rent} onChange={handlechange} id="rent" className="w-5"/>
                             <span>Rent</span>
                         </div>
                         <div className='flex items-center gap-3'>
-                            <input type="checkbox" id="parking" className="w-5"/>
+                            <input type="checkbox" checked={formData.parking} onChange={handlechange} id="parking" className="w-5"/>
                             <span>Parking</span>
                         </div>
                         <div className='flex items-center gap-3'>
-                            <input type="checkbox" id="furnished" className="w-5"/>
+                            <input type="checkbox" checked={formData.furnished} onChange={handlechange} id="furnished" className="w-5"/>
                             <span>Furnished</span>
                         </div>
                         <div className='flex items-center gap-3'>
-                            <input type="checkbox" id="offer" className="w-5"/>
+                            <input type="checkbox" onChange={handlechange} checked={formData.offer} id="offer" className="w-5"/>
                             <span>Offer</span>
                         </div>
                     </div>
                         <div className='flex gap-3 flex-wrap'>
                             <div className='flex gap-2 items-center'> 
-                                <input type="number"  id="bedRoom" className='p-3 rounded-lg border-gray-100'></input>
+                                <input type="number" onChange={handlechange} value={formData.bedRoom} id="bedRoom" className='p-3 rounded-lg border-gray-100'></input>
                                 <p>Beds</p>
                             </div>
                             <div className='flex gap-3 items-center'> 
-                                <input type="number"  id="bathRoom" className='p-3 rounded-lg border-gray-100'></input>
+                                <input type="number"  id="bathRoom" onChange={handlechange} value={formData.bathRoom} className='p-3 rounded-lg border-gray-100'></input>
                                 <p>Baths</p>
                             </div>
                             <div className='flex gap-3 items-center'> 
-                                <input type="number"  id="regularPrice" className='p-3 rounded-lg border-gray-100'></input>
+                                <input type="number" onChange={handlechange} value={formData.regularPrice} id="regularPrice" className='p-3 rounded-lg border-gray-100'></input>
                                 <p>Regular Price</p>
                                 <div>
                                     <span>($/month)</span>
                                 </div>
                             </div>
                             <div className='flex gap-3 items-center'> 
-                                <input type="number"  id="discountPrice" className='p-3 rounded-lg border-gray-100'></input>
+                                <input type="number"  id="discountPrice" onChange={handlechange} value={formData.discountPrice} className='p-3 rounded-lg border-gray-100'></input>
                                 <p >Discount Price</p>
                                 <div>
                                     <span>($/month)</span>
@@ -75,4 +99,5 @@ export default function CreateListing() {
         </div>
     </main>
   )
-}
+    }
+
